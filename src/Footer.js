bd26b092ce8d24c6544728656dba2a4a303d1b59
Dairenji <37531253+ntokozokubheka@users.noreply.github.com>
2023-04-12 12:37:01 +0000

    repo creation

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Footer =()=>{
+   
+   return <div>
+    
+    <footer>
+    <p>Author: Hege Refsnes<br></br>
+     <a href="mailto:hege@example.com">hege@example.com</a></p>
+     </footer> 
+    </div>
+  };
+
+
+  export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header =()=>{
+    return <div>
+    <div className="topnav">
+    <a className="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+     </div>
+  
+    </div>;
+  };
+  export default Header;
