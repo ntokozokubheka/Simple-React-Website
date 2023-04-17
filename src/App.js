b3df5164f3ef5f401924a1ab9433c8b3b68e5f6a
Dairenji <37531253+ntokozokubheka@users.noreply.github.com>
2023-04-17 11:42:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from "../components/Header";
-import Body from "../components/Body";
-import Footer from "../components/Footer";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 
 function App() {
